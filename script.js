1c80907454a2412d7538b928a25d36b29d1eab21
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-04-03 19:35:57 +0100

    Set up template cards

---
+++ script.js
@@ -0,0 +1,14 @@
+const displayBooks = document.querySelector(".books-grid");
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
