8662982db94a8b348b388c0a40c144a2c2e7ff5b
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-04-06 16:32:45 +0100

    Write read me

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# library
+# Library
+
+## [Live Demo](https://erinsophie.github.io/library/)
+
+![Admin Dashboard](images/library.png)
+
+# Technologies Used 
+
+- HTML
+- CSS
+- Javascript
+
+# Project goals
+
+- Create a personal library where users can add books to their collection
+- The main idea was to learn how object constructors work and the different ways we can set an object's prototype. Particularly how Javascript is unqiue in that it uses a prototype-based inheritence model as opposed to many other languages which use a class-based inheritence model
+- The library allows users to add books represented by cards which are displayed using CSS Grid, allowing the layout to respond well to different-sized screens
+- The library lets users toggle the read status and remove books from the collection
+- The form does not allow users to add books that already exist in the collection and requires all fields to be filled out
+- When a book is added, the first letter of the title and author will be automatically capitalised
+
+
--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 // my library array of objects 
-
 const myLibrary = [];
 
 // books grid 
-
 const booksGrid = document.querySelector('.books-grid');
 
 // capitalise first letter of input 
-
 function capitaliseLetter(str) {
-  const splitStr = str.split(' ')
-  const capitalised = splitStr.map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  return capitalised.join(' ')
+  const splitStr = str.split(' ');
+  const capitalised = splitStr.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return capitalised.join(' ');
 }
 
 // check if library is empty
-
 function isLibraryEmpty() {
   if(myLibrary.length === 0) {
     return true;
   } 
-  return false 
+  return false;
 }
 
 function messageStatus() {
   const message = document.querySelector('.empty-msg');
-
   if(!isLibraryEmpty()) {
     message.style.display = 'none';
   } else {
@@ -36,7 +31,6 @@ function messageStatus() {
 
 
 // create card element for the each book
-
 function displayBook(book, index) {
     const card = document.createElement('div');
     card.classList.add('card');
@@ -93,15 +87,13 @@ function displayBook(book, index) {
     card.appendChild(btnContainer);
   
     booksGrid.appendChild(card);
-
     messageStatus();
   };
 
 
 
-    
-// make form pop up when add book button is clicked
 
+// make form pop up when add book button is clicked
 const modal = document.querySelector('.modal');
 const addBookBtn = document.querySelector('.add-btn')
 const overlay = document.querySelector('.overlay')
@@ -113,8 +105,8 @@ addBookBtn.addEventListener('click', () => {
   formInputs.reset();
 })
 
-// close modal
 
+// close modal
 function closeModal() {
   modal.classList.remove('active');
   overlay.classList.remove('active');
@@ -122,8 +114,8 @@ function closeModal() {
 
 overlay.addEventListener('click', closeModal);
 
-// constructor 
 
+// constructor 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -132,7 +124,6 @@ function Book(title, author, pages, read) {
 }
 
 // check library in case book already exists 
-
 function checkLibrary(book) {
   for(let i = 0; i < myLibrary.length; i++) {
     if(book.title === myLibrary[i].title) {
@@ -140,19 +131,17 @@ function checkLibrary(book) {
       return true;
     }
   }
-  return false 
+  return false;
 }
 
 
 
-
 // Add book to library when form button is clicked
-
 function addBook(event) {
   event.preventDefault();
 
   if (!formInputs.checkValidity()) {
-    alert('Please fill out all fields')
+    alert('Please fill out all fields');
     return;
   }
   
@@ -182,21 +171,15 @@ function addBook(event) {
 
 
 // form button
-
 const formBtn = document.querySelector('.form-btn')
 formBtn.addEventListener('click', addBook);
 
 // define read status function on prototype 
-
 Book.prototype.toggleReadStatus = function() {
   this.read = !this.read;
 };
 
 // define remove book function on prototype
-
 Book.prototype.removeBook = function(index) {
   myLibrary.splice(index, 1)
 }
-
-
-
